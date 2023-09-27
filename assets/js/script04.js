979b85a94370d9312c96cc960f152d3e7e1d5617
bezabu <>
2023-09-27 21:44:58 +0000

    github filepaths

--- assets/js/script04.js
+++ assets/js/script04.js
@@ -10,7 +10,7 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
-playerImage.src = './assets/images/shadow_dog.png';
+playerImage.src = 'https://bezabu.github.io/bb-js-experiment/assets/images/shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
 
