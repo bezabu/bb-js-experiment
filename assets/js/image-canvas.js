8f43b099898028dc4623afafdd4328ec41c6c49f
bezabu <>
2023-09-27 21:32:44 +0000

    github debugging filepaths

--- assets/js/image-canvas.js
+++ assets/js/image-canvas.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle = "#000000";
 ctx.fillRect(0, 0, 800, 450);
 const img = new Image(); // Create new img element
-img.src = "../assets/images/tree01.png"; // Set source path
+img.src = "../images/tree01.png"; // Set source path
 img.onload = () => {
     ctx.drawImage(img, 10, 10);
 };
